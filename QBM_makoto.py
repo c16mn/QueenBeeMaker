1e73694383c2fd9cd8fc6d848f99074921b4c6fc
Makoto Nara <c16mn@users.noreply.github.com>
2021-12-28 11:55:59 -0500

    added brute force solution

--- QBM_makoto.py
+++ QBM_makoto.py
@@ -1,2 +1,34 @@
+import sys
+
+from dict_ref import dref
+
+l = sys.argv[1]
+c = sys.argv[2]
 
 
+def solve(letters, center):
+    if len(letters) != 7 or len(set(letters)) != 7:
+        return "didn't enter 7 unique characters for surrounding letters"
+    if len(center) != 1:
+        return "didn't enter 1 character for center letter"
+    if center not in letters:
+        return "center letter not a part of overall letters"
+    
+    letters = ''.join(sorted(letters))
+
+    lset = set(letters)
+
+    ans = []
+
+    with open("dict_short.txt", "r") as dr:
+        for line in dr:
+            if set(line.lower().strip()).issubset(lset):
+                ans.append(line)
+
+    return ans
+    
+    #with open "dict_short.txt" as d:
+
+if __name__ == "__main__":
+    print(solve(l, c))
+    
--- dict-filter.py
+++ dict-filter.py
@@ -1,6 +1,24 @@
+import json
 
-with open("scrabble.txt", "r") as dreader, open("dict_short.txt", "w") as dwriter:
-    for line in dreader:
-        if (len(line.strip()) >= 4 and len(set(line.strip())) <= 7):
-            dwriter.write(line)
+# with open("scrabble.txt", "r") as dreader, open("dict_short.txt", "w") as dwriter:
+#     for line in dreader:
+#         if (len(line.strip()) >= 4 and len(set(line.strip())) <= 7):
+#             dwriter.write(line)
+
+dref = {"AA": 0}
+with open("dict_short.txt", "r") as ds:
+	linecounter = 0
+	for line in ds:
+		head = line[0:2]
+		if head not in dref:
+			dref[head] = linecounter
+		linecounter = linecounter + 1
+
+with open("dict_ref.py", "w") as dr:
+	dr.write("dref = ")
+	dr.write(json.dumps(dref))
+
+
+
+
 
